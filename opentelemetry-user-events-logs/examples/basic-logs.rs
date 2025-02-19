6752a4e9791cf00357f984ba0cca275ad71fc209
Cijo Thomas <cijo.thomas@gmail.com>
2025-02-19 11:48:09 -0800

    build

--- opentelemetry-user-events-logs/examples/basic-logs.rs
+++ opentelemetry-user-events-logs/examples/basic-logs.rs
@@ -29,7 +29,7 @@ fn main() {
     let otel_layer = layer::OpenTelemetryTracingBridge::new(&logger_provider);
     let otel_layer = otel_layer.with_filter(filter_otel);
 
-    let filter_fmt = EnvFilter::new("info").add_directive("opentelemetry=debug".parse().unwrap()).add_directive("basic_logs=off".parse().unwrap());;
+    let filter_fmt = EnvFilter::new("info").add_directive("opentelemetry=debug".parse().unwrap()).add_directive("basic_logs=off".parse().unwrap());
     let fmt_layer = tracing_subscriber::fmt::layer().with_filter(filter_fmt);
 
     tracing_subscriber::registry()
